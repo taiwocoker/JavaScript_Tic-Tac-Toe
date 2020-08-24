02c58f91714f30cd1689058827ba4d80a6370752
Manezeu Patricia <pmanezeu@gmail.com>
2020-08-24 17:02:59 +0100

    Created the game object

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,10 @@ function Player(name, sign){
     return {name, sign, addChoice}
 }
 
-function board(){
+function Board(){
     const positions = [1,2,3,4,5,6,7,8,9];
     const winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
     const filledBoard = function(){
         let res = true;
         positions.forEach(element => {
@@ -36,5 +37,34 @@ function board(){
         return winningCombinations.includes(choices);
     }
     
+    return { filledBoard, updateBoard, winningComb }
+}
+
+function Game(player1, player2) {
+    const playerOne = Player(player1, 'heart');
+    const playerTwo = Player(player2, 'cross');
+    const board = Board();
+
+    const play = function(position, player) {
+        return board.updateBoard(position, player.sign);
+    }
+
+    const winner = function() {
+        let winner = false;
+        if (board.winningComb(playerOne.choices)){
+            winner = playerOne;
+        }
+        if (board.winningComb(playerTwo.choices)){
+            winner = playerTwo;
+        }
+        return winner;
+    }
+
+    const start = function() {
+        let currentPlayer = playerOne;
+        while (board.filledBoard() !== false){
+            choice = DomAction.getChoice();
+        }
+    }
 }
 
